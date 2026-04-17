429978a507e757bc0364be015a57424b48735461
Gabriel Marinho <gabriellgarcia1966@gmail.com>
2026-04-16 22:55:56 -0300

    adicionando titulo de cabeçalho na janela

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ janela.title("Lista de Tarefas")
 janela.configure(bg="#F0F0F0")
 janela.geometry("500x500")
 
+# Cabeçalho da janela
+fonte_cabecalho = font.Font(family="Arial", size=24, weight="bold")
+titulo_cabelaho = tk.Label(janela, text="Lista de Tarefas", font=fonte_cabecalho, bg="#F0F0F0", fg="#333").pack(pady=20)
+
+
 # Loop principal do app
 janela.mainloop()
