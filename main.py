08aeee5010867298df8933d57918fb24483b6c07
Gabriel Marinho <gabriellgarcia1966@gmail.com>
2026-04-16 22:41:27 -0300

    criação da janela

--- main.py
+++ main.py
@@ -2,4 +2,10 @@ import tkinter as tk
 from tkinter import ttk, font, messagebox
 from tkinter import PhotoImage
 
-aa
+# Criando a janela do app
+janela = tk.Tk()
+janela.title("Lista de Tarefas")
+janela.geometry("500x500")
+
+# Loop principal do app
+janela.mainloop()
