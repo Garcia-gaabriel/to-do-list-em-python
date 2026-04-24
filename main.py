8d2603af01965b202a33e8253f092847c9828179
Gabriel Marinho <gabriellgarcia1966@gmail.com>
2026-04-24 20:45:37 -0300

    Teste 01 - arquivo pertinencias funcionando corretamente

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ from tarefa import Tarefa
 from persistencia import carregar_tarefas, salvar_tarefas
 
 tarefa1 = Tarefa("Estudar Python", "Vendo video aulas no youtube", "24/04/2026")
-print(tarefa1)
-print(tarefa1.detalhes())
+
+salvar_tarefas([tarefa1])
+
+tarefas = carregar_tarefas()
+print(tarefas[0].detalhes())
