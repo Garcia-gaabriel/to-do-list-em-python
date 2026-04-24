300a29e14b7c75352d0f1debc81a62712d291d3b
Gabriel Marinho <gabriellgarcia1966@gmail.com>
2026-04-24 20:17:53 -0300

    Primeiro debug, tudo rodando normalmente ;)

---
+++ main.py
@@ -0,0 +1,5 @@
+from tarefa import Tarefa
+
+tarefa1 = Tarefa("Estudar Python", "Vendo video aulas no youtube", "24/04/2026")
+print(tarefa1)
+print(tarefa1.detalhes())
--- tarefa.py
+++ tarefa.py
@@ -11,7 +11,27 @@ class Tarefa:
         self.status = status
 
     def __repr__(self):
-        return f"Tarefa(titulo='{self.titulo}'  )"
+        return f"Tarefa(titulo='{self.titulo}', descricao='{self.descricao}', " \
+               f"data_vencimento='{self.data_vencimento.strftime('%d/%m/%Y')}', status='{self.status}')"
+    
+    # Marca tarefa como concluída
+    def marcar_concluida(self):
+        self.status = "Concluída"
+
+    # Registra tarefa como atrasada se a data atual é maior do que a de vencimento
+    # e se o status dela é Pendete... 
+    def esta_atrasada(self):
+        return datetime.now() > self.data_vencimento and self.status == "Pendente"
+    
+    # Definindo o retorno para o usuario
+    def detalhes(self):
+        status = "Atrasada" if self.esta_atrasada() else self.status
+        return (f"Título: {self.titulo}\n"
+                f"Descrição: {self.descricao}\n"
+                f"Data de vencimento: {self.data_vencimento.strftime('%d/%m/%Y')}\n"
+                f"Status: {self.status}")
+    
+
 
     
     
