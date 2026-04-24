1f81dcb088255e924fd636d95296b837abffdb26
Gabriel Marinho <gabriellgarcia1966@gmail.com>
2026-04-24 19:52:25 -0300

    Mudand o código para outro formato

--- tarefa.py
+++ tarefa.py
@@ -1,3 +1,17 @@
+from datetime import datetime
+
 class Tarefa:
 
-    def __init__(self, )
+    def __init__(self, titulo, descricao, data_vencimento, status="Pendente"):
+        self.titulo = titulo
+        self.descricao = descricao
+        # Trasnforma a string de data de fato em um objeto no formato data (datetime)
+        # Formato utilizado vai ser: dia/mes/ano
+        self.data_vencimento = datetime.strptime(data_vencimento, "%d/%m/%Y")
+        self.status = status
+
+    def __repr__(self):
+        return f"Tarefa(titulo='{self.titulo}'  )"
+
+    
+    
