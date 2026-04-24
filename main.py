16f3792e8d9d590e5bf992b3b694e59dea560bd4
Gabriel Marinho <gabriellgarcia1966@gmail.com>
2026-04-24 20:40:59 -0300

    Arquivo persistencia criado

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tarefa import Tarefa
+from persistencia import carregar_tarefas, salvar_tarefas
 
 tarefa1 = Tarefa("Estudar Python", "Vendo video aulas no youtube", "24/04/2026")
 print(tarefa1)
---
+++ persistencia.py
@@ -0,0 +1,23 @@
+import pickle
+
+# Função para salvar as tarefas
+def salvar_tarefas(tarefas, nome_arquivo="tarefas.pkl"):
+    with open(nome_arquivo, "wb") as arquivo:
+        pickle.dump(tarefas, arquivo)
+    print(f"Tarefas salvas em '{nome_arquivo}' com sucesso!")
+
+# Função para abrir as tarefas
+def carregar_tarefas(nome_arquivo="tarefas.pkl"):
+    try:
+        with open(nome_arquivo, "rb") as arquivo:
+            tarefas = pickle.load(arquivo)
+        print(f"Tarefas de '{nome_arquivo}' carregadas com sucesso!")
+        return tarefas
+    
+    # Tratamento de erros
+    except FileNotFoundError:
+        print(f"Arquivo '{nome_arquivo}' não encontrado. Nenhuma tarefa foi carregada")
+        return []
+    except EOFError:
+        print(f"O arquivo '{nome_arquivo}' está vazio. Nenhuma tarefa foi carregada")
+        return []
